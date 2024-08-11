bb808211474f68b9ae3e1219496e07b823aa3205
Joao Vitor <joaovitorshenriques@gmail.com>
2024-08-11 03:19:19 -0300

    feat: adicionando funcionalidades de salvar, inserir, ler e imprimir os clubes

--- Primeira-Parte-Projeto/clube.c
+++ Primeira-Parte-Projeto/clube.c
@@ -13,3 +13,52 @@ TClube *criarClube(int id, char *nome){
     strcpy(clube->nome, nome);
     return clube;
 }
+
+void salvaClube(TClube *clube, FILE *out){
+    fwrite(&clube->id, sizeof(int), 1, out);
+    fwrite(clube->nome, sizeof(char), sizeof(clube->nome), out);
+}
+
+void *leClube(FILE *out){
+    TClube *clube = (TClube *) malloc(sizeof(TClube));
+    if (0 >= fread(&clube->id, sizeof(int), 1, out)) {
+        free(clube);
+        return NULL;
+    }
+    fread(clube->nome, sizeof(char), sizeof(clube->nome), out);
+    return clube;
+}
+
+void insereClubes(FILE *out){
+
+    int ids [] = {4,11,9,1,15,10,2,3,7,17,5,20,13,6,16,12,19,8,14,18};
+    char *nomes[ ] = {"Botafogo","Flamengo","Fortaleza","Palmeiras","Cruzeiro","Sao Paulo","Bahia","CAP","Atletico-MG","Bragantino","Vasco","Criciuma","Juventude","Gremio","Vitoria","Internacional","Fluminense","Corinthians","Cuiaba","Atletico-GO"};
+
+    int numClubes = sizeof(ids)/sizeof(ids[0]);
+
+    for (int i = 0; i < numClubes; i++){
+        TClube*clube = criarClube(ids[i],nomes[i]);
+        salvaClube(clube,out);
+        free(clube);
+        fflush(out);
+    }
+}
+
+void leClubesEImprime(FILE *in){
+    printf("\n\nLendo clubes do arquivo...\n\n");
+    rewind(in);
+    TClube *c;
+    while ((c = leClube(in)) != NULL){
+        imprimeClube(c);
+        free(c);
+    }
+}
+
+void imprimeClube(TClube *clube){
+    printf("\n------CLUBE------");
+	printf("\nCodigo do Clube: ");
+    printf("%d", clube->id);
+	printf("\nNome do Clube: ");
+    printf("%s", clube->nome);
+    printf("\n-------------------------------\n");
+}
--- Primeira-Parte-Projeto/clube.h
+++ Primeira-Parte-Projeto/clube.h
@@ -5,3 +5,16 @@ typedef struct Clube{
     int id;
     char nome[100];
 } TClube;
+
+
+TClube *criarClube(int id, char *nome);
+
+void salvaClube(TClube *clube, FILE *out);
+
+void *leClube(FILE *out);
+
+void insereClubes(FILE *out);
+
+void imprimeClube(TClube *clube);
+
+void leClubesEImprime(FILE *in);
